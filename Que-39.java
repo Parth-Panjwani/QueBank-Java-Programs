4c255211e72bab20a18b5e61b8a6eab85a584697
Parth  Panjwani <81768790+Parth-Panjwani@users.noreply.github.com>
2022-04-23 03:22:23 +0530

    Updated

--- Que-39.java
+++ Que-39.java
@@ -1,51 +1,32 @@
 //Write a program which create two threads and then generates numbers from 15 to 1 for the delay of 1sec between them. Display the thread details for the current thread and changed thread name to thread1 to “San” and thread2 to “Bag”.
 //By: Parth Panjwani
-class Que39
-{
-    public static void main(String[] args) throws InterruptedException
-    {
-        Thread t1=new Thread(new Runnable()
-        {
-            public void run()
-            {
-                for(int i=15;i>=1;i--)
-                {
-                    System.out.println(i);
-                    try
-                    {
-                        Thread.sleep(1000);
-                    }
-                    catch(InterruptedException e)
-                    {
-                        System.out.println("Thread interrupted");
-                    }
+class myThread extends Thread {
+    public void run() {
+        synchronized (myThread.class){
+            System.out.println( "\n" + this.getName() );
+            for( int i=15; i>=0; i-- ){
+                System.out.print(i + " ");
+                try{
+                    this.sleep(1000);
+                }
+                catch(Exception e){
+                    System.out.println("ERROR");
                 }
             }
-        });
-        Thread t2=new Thread(new Runnable()
-        {
-            public void run()
-            {
-                for(int i=15;i>=1;i--)
-                {
-                    System.out.println(i);
-                    try
-                    {
-                        Thread.sleep(1000);
-                    }
-                    catch(InterruptedException e)
-                    {
-                        System.out.println("Thread interrupted");
-                    }
-                }
-            }
-        });
-        t1.start();
-        t2.start();
+        }
+    };
+}
+
+
+public class Main9 {
+    public static void main(String[] args) {
+        myThread t1 = new myThread();
+        myThread t2 = new myThread();
+
         t1.setName("San");
         t2.setName("Bag");
-        System.out.println("Current thread name is "+Thread.currentThread().getName());
-        System.out.println("Thread1 name is "+t1.getName());
-        System.out.println("Thread2 name is "+t2.getName());
+
+        t1.start();
+        t2.start();
     }
 }
